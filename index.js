656f0212b7e251836e13ef7398f16715e4103cac
deaszy <brampeer15@gmail.com>
2023-02-07 00:39:48 +0100

    update

--- index.js
+++ index.js
@@ -164,3 +164,18 @@ const updateProperties = (elem, state) => {
   })
   
 
+  gsap.from(".bar abbr", {
+    scrollTrigger: ".bar",
+    left: 0,
+    ease: Power2.easeInOut,
+    duration: 3,
+    stagger: 0.1
+});
+gsap.from(".bar span", {
+    scrollTrigger: ".bar",
+    width: "0px",
+    ease: Power2.easeInOut,
+    duration: 3,
+    stagger: 0.1
+});
+
